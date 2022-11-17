5bdb02567fdbed71287c4aeec5fd545e4fe02401
João P. Reis <105528742+kxxnz@users.noreply.github.com>
2022-11-17 08:06:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var screen = document.querySelector('#screen');
+document.querySelector('#limpar')
+    .addEventListener('click', function() {
+        screen.value = '';
+    })
+document.querySelector('#apagar')
+    .addEventListener('click', function() {
+        screen.value = screen.value.substring(0, (screen.value.length - 1));
+    })
+
+function addToScreen(value) {
+    const operationSigns = ['*', '+', '-', '/', '.'];
+    screen.value += value;
+    if (operationSigns.includes(value)) {
+        var valorAnterior = screen.value.length - 2;
+        if (operationSigns.includes(screen.value[valorAnterior])) {
+            screen.value = screen.value.substring(0, (screen.value.length - 2));
+            screen.value = screen.value + value;
+        }
+    }
+    switch (screen.value[0]) {
+        case '*':
+            screen.value = '';
+        case '/':
+            screen.value = '';
+        case '.':
+            screen.value = '';
+    }
+}
+
+function result() {
+    screen.value = eval(screen.value);
+}
